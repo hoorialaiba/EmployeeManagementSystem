fa28f39d4fa21864bf4e7678c41d95dadb6fe168
hoorialaiba <hoorialaiba123@gmail.com>
2026-02-03 19:19:00 +0500

    Add files via upload

---
+++ DAL/DepartmentDAL.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+
+namespace DAL
+{
+    public class DepartmentDAL
+    {
+        public List<DepartmentDTO> GetAll()
+        {
+            StreamReader sr = new StreamReader("department.txt");
+            string data = sr.ReadLine();
+            List<DepartmentDTO> list = new List<DepartmentDTO>();
+            while (data != null)
+            {
+                DepartmentDTO et = new DepartmentDTO();
+                string[] st = data.Split(',');
+                et.Id = int.Parse(st[0]);
+                et.Name = st[1];
+                et.Description = st[2];
+                list.Add(et);
+                data = sr.ReadLine();
+
+            }
+            sr.Close();
+            return list;
+        }
+        public void add(DepartmentDTO e)
+        {
+            StreamWriter sw = new StreamWriter("department.txt", true);
+            string data = $"{e.Id},{e.Name},{e.Description}";
+            sw.WriteLine(data);
+            sw.Close();
+        }
+        public void del(DepartmentDTO e)
+        {
+            StreamReader sr = new StreamReader("department.txt");
+            string data = sr.ReadLine();
+            List<DepartmentDTO> list = new List<DepartmentDTO>();
+            while (data != null)
+            {
+                DepartmentDTO et = new DepartmentDTO();
+                string[] st = data.Split(',');
+                et.Id = int.Parse(st[0]);
+                et.Name = st[1];
+                et.Description = st[2];
+                if (e.Name != et.Name)
+                {
+                    list.Add(et);
+                }
+                data = sr.ReadLine();
+
+            }
+            sr.Close();
+            StreamWriter sw = new StreamWriter("department.txt");
+            foreach (DepartmentDTO emp in list)
+            {
+                data = $"{emp.Id},{emp.Name},{emp.Description}";
+                sw.WriteLine(data);
+            }
+            sw.Close();
+        }
+        public void editDept(DepartmentDTO e, string des)
+        {
+            StreamReader sr = new StreamReader("department.txt");
+            string data = sr.ReadLine();
+            List<DepartmentDTO> list = new List<DepartmentDTO>();
+            while (data != null)
+            {
+                DepartmentDTO et = new DepartmentDTO();
+                string[] st = data.Split(',');
+                et.Id = int.Parse(st[0]);
+                et.Name = st[1];
+                et.Description = st[2];
+                if (e.Name == et.Name)
+                {
+                    et.Description = des;
+                }
+                list.Add(et);
+                data = sr.ReadLine();
+
+            }
+            sr.Close();
+            StreamWriter sw = new StreamWriter("department.txt");
+            foreach (DepartmentDTO emp in list)
+            {
+                data = $"{emp.Id},{emp.Name},{emp.Description}";
+                sw.WriteLine(data);
+            }
+            sw.Close();
+        }
+    }
+}
---
+++ DAL/EmployeeDAL.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+
+namespace DAL
+{
+    public class EmployeeDAL
+    {
+        public List<EmployeeDTO> GetAll() 
+        {
+            StreamReader sr = new StreamReader("employee.txt");
+            string data = sr.ReadLine();
+            List<EmployeeDTO> list = new List<EmployeeDTO>();
+            while (data != null)
+            {
+                EmployeeDTO et = new EmployeeDTO();
+                string[] st = data.Split(',');
+                et.Id = st[0];
+                et.Name = st[1];
+                et.Age = int.Parse(st[2]);
+                et.Department = st[3];
+                et.Salary = float.Parse(st[4]);
+                et.Date = st[5];
+                list.Add(et);
+                data = sr.ReadLine();
+
+            }
+            sr.Close();
+            return list;
+        }
+        public void add(EmployeeDTO e) 
+        {
+            StreamWriter sw = new StreamWriter("employee.txt", true);
+            string data = $"{e.Id},{e.Name},{e.Age},{e.Department},{e.Salary},{e.Date}";
+            sw.WriteLine(data);
+            sw.Close();
+        }
+        public void del(EmployeeDTO e)
+        {
+            StreamReader sr = new StreamReader("employee.txt");
+            string data = sr.ReadLine();
+            List<EmployeeDTO> list = new List<EmployeeDTO>();
+            while (data != null)
+            {
+                EmployeeDTO et = new EmployeeDTO();
+                string[] st = data.Split(',');
+                et.Id = st[0];
+                et.Name = st[1];
+                et.Age = int.Parse(st[2]);
+                et.Department = st[3];
+                et.Salary = float.Parse(st[4]);
+                et.Date = st[5];
+                if (e.Id != et.Id)
+                {
+                    list.Add(et);
+                }
+                data = sr.ReadLine();
+
+            }
+            sr.Close();
+            StreamWriter sw = new StreamWriter("employee.txt");
+            foreach (EmployeeDTO emp in list)
+            {
+                data = $"{emp.Id},{emp.Name},{emp.Age},{emp.Department},{emp.Salary},{emp.Date}";
+                sw.WriteLine(data);
+            }
+            sw.Close();
+        }
+        public void updateDName(EmployeeDTO e, string d)
+        {
+            StreamReader sr = new StreamReader("employee.txt");
+            string data = sr.ReadLine();
+            List<EmployeeDTO> list = new List<EmployeeDTO>();
+            while (data != null)
+            {
+                EmployeeDTO et = new EmployeeDTO();
+                string[] st = data.Split(',');
+                et.Id = st[0];
+                et.Name = st[1];
+                et.Age = int.Parse(st[2]);
+                et.Department = st[3];
+                et.Salary = float.Parse(st[4]);
+                et.Date = st[5];
+                if (e.Id == et.Id)
+                {
+                    et.Department = d;
+                }
+                list.Add(et);
+                data = sr.ReadLine();
+
+            }
+            sr.Close();
+            StreamWriter sw = new StreamWriter("employee.txt");
+            foreach (EmployeeDTO emp in list)
+            {
+                data = $"{emp.Id},{emp.Name},{emp.Age},{emp.Department},{emp.Salary},{emp.Date}";
+                sw.WriteLine(data);
+            }
+            sw.Close();
+        }
+        public void updateS(EmployeeDTO e, float s)
+        {
+            StreamReader sr = new StreamReader("employee.txt");
+            string data = sr.ReadLine();
+            List<EmployeeDTO> list = new List<EmployeeDTO>();
+            while (data != null)
+            {
+                EmployeeDTO et = new EmployeeDTO();
+                string[] st = data.Split(',');
+                et.Id = st[0];
+                et.Name = st[1];
+                et.Age = int.Parse(st[2]);
+                et.Department = st[3];
+                et.Salary = float.Parse(st[4]);
+                et.Date = st[5];
+                if (e.Id == et.Id)
+                {
+                    et.Salary = s;
+                }
+                list.Add(et);
+                data = sr.ReadLine();
+
+            }
+            sr.Close();
+            StreamWriter sw = new StreamWriter("employee.txt");
+            foreach (EmployeeDTO emp in list)
+            {
+                data = $"{emp.Id},{emp.Name},{emp.Age},{emp.Department},{emp.Salary},{emp.Date}";
+                sw.WriteLine(data);
+            }
+            sw.Close();
+        }
+    }
+}
